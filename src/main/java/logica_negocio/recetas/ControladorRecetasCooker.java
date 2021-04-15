5ae527c9bd28a60f1f42fb19fe3456d3d4cdf876
Estefania Bermudez Arroyo <estefania_bermudeza@javeriana.edu.co>
2021-04-14 23:47:52 -0500

    Create interface IControladorRecetasCooker.

--- src/main/java/logica_negocio/recetas/ControladorRecetasCooker.java
+++ src/main/java/logica_negocio/recetas/ControladorRecetasCooker.java
@@ -1,4 +1,29 @@
 package logica_negocio.recetas;
 
-public class ControladorRecetasCooker {
+import java.util.ArrayList;
+import java.util.List;
+import entidades.modelo.Receta;
+import entidades.modelo.Cooker;
+
+public class ControladorRecetasCooker implements IControladorRecetasCooker{
+
+    private Cooker cooker;
+
+    public ControladorRecetasCooker() {
+
+    }
+
+    public ControladorRecetasCooker(Cooker cooker) {
+        this.cooker = cooker;
+    }
+
+    public Cooker getCooker() {
+        return cooker;
+    }
+
+    public void setCooker(Cooker cooker) {
+        this.cooker = cooker;
+    }
+
+
 }
