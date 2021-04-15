34d5c00d4bb433e5449ee94fb2123ed482cb1e7f
Alejandro Morales <a.moralesc@javeriana.edu.co>
2021-04-15 01:01:15 -0500

    Create auth security controllers

---
+++ src/main/java/logica_negocio/autenticacion_registro/ControladorSeguridad.java
@@ -0,0 +1,51 @@
+package logica_negocio.autenticacion_registro;
+
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class ControladorSeguridad implements IControladorSeguridad {
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String generarHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        int iteraciones = 1000;
+        char[] chars = password.toCharArray();
+
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iteraciones, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return toHex(hash);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public byte[] generarSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    /**
+     * Convierte un número representado en bytes a hex
+     * @param arreglo el arreglo de bytes a pasar a hex
+     * @return el String en hex
+     */
+    private String toHex(byte[] arreglo) {
+        BigInteger bi = new BigInteger(1, arreglo);
+        String hex = bi.toString(16);
+        int longitudRelleno = (arreglo.length * 2) - hex.length();
+        if (longitudRelleno > 0) {
+            return String.format("%0" + longitudRelleno + "d", 0) + hex;
+        } else {
+            return hex;
+        }
+    }
+}
---
+++ src/main/java/logica_negocio/autenticacion_registro/IControladorSeguridad.java
@@ -0,0 +1,23 @@
+package logica_negocio.autenticacion_registro;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public interface IControladorSeguridad {
+    /**
+     * Encripta una contraseña (con salt) mediante el algoritmo de encriptación PBKFD2, proporcionado por JavaX
+     * @param password la contraseña a encriptar
+     * @param salt el salt de la contraseña
+     * @return el hash encriptado
+     * @throws NoSuchAlgorithmException ver también {@link javax.crypto.SecretKeyFactory#getInstance(String) getInstance}
+     * @throws InvalidKeySpecException ver también {@link javax.crypto.SecretKeyFactory#getInstance(String) getInstance}
+     */
+    public String generarHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException;
+
+    /**
+     * Genera una salt al azar de manera segura (mediante SHA1PRNG) de 16 bytes
+     * @return el salt generado
+     * @throws NoSuchAlgorithmException ver también {@link java.security.SecureRandom#getInstance(String) getInstance}
+     */
+    public byte[] generarSalt() throws NoSuchAlgorithmException;
+}
