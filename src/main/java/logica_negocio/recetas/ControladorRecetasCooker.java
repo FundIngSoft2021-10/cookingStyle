a373d8da045eccf1b35446e52f26bfc3aa9942e5
Estefania Bermudez Arroyo <estefania_bermudeza@javeriana.edu.co>
2021-04-15 21:15:58 -0500

    Create methods of crearListaFavoritos

--- src/main/java/logica_negocio/recetas/ControladorRecetasCooker.java
+++ src/main/java/logica_negocio/recetas/ControladorRecetasCooker.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import entidades.dto.DTOReceta;
 import entidades.dto.DTORecetaMiniatura;
+import entidades.modelo.ListaFavoritos;
 import entidades.modelo.Receta;
 import entidades.modelo.Cooker;
 
@@ -28,10 +29,72 @@ public class ControladorRecetasCooker implements IControladorRecetasCooker{
         this.cooker = cooker;
     }
 
-    public DTOReceta verReceta(DTORecetaMiniatura dtoRecetaMiniatura){
-        
-        DTOReceta dtoReceta = new DTOReceta();
-        return dtoReceta;
+    //Métodos Funcionalidad Crear Lista Favoritos
+
+
+    /**
+     * Creación de una lista de recetas favorita específica con una receta inicial
+     * @param nombreLista nombre de la lista de favoritos
+     * @param descripcion decripcion de la lista
+     * @param receta receta a añiadir a la lista
+     * @return true si se creo con éxito, false si no se creó
+     */
+    @Override
+    public boolean crearListaFavoritos(String nombreLista, String descripcion, Receta receta) {
+
+        try {
+
+            //Se obtiene el id de la nueva lista
+            int id = cooker.getListasFavoritos().size() + 1;
+
+            //Se añade la receta a una lista de recetas
+            List<Receta> listaReceta = new ArrayList<>();
+            listaReceta.add(receta);
+
+            //Se crea la lista de recetas
+            ListaFavoritos listaFavoritos = new ListaFavoritos(id, nombreLista, descripcion, listaReceta);
+            //Se añade la lista a la lista de favoritos del cooker
+            cooker.getListasFavoritos().add(listaFavoritos);
+
+            //La nueva receta favorita, se añade a la lista de favoritos general
+            for(ListaFavoritos listaFavoritos1 : cooker.getListasFavoritos()){
+                if(listaFavoritos1.getIdListaFavoritos() == 1){
+                    listaFavoritos1.getRecetasFavoritas().add(receta);
+                }
+            }
+
+        } catch (Exception e){
+            return false;
+        }
+        return false;
+
+    }
+
+    /**
+     * Creación de una lista de recetas favorita específica sin una receta inicial
+     * @param nombreLista nombre de la lista de favoritos
+     * @param descripcion decripcion de la lista
+     * @return true si se creo con éxito, false si no se creó
+     */
+    @Override
+    public boolean crearListaFavoritos(String nombreLista, String descripcion) {
+        try {
+
+            //Se obtiene el id de la nueva lista
+            int id = cooker.getListasFavoritos().size() + 1;
+
+            //Se crea una lista de recetas
+            List<Receta> listaRecetas = new ArrayList<>();
+
+            //Se crea la lista de recetas
+            ListaFavoritos listaFavoritos = new ListaFavoritos(id, nombreLista, descripcion, listaRecetas);
+            //Se añade la lista a la lista de favoritos del cooker
+            cooker.getListasFavoritos().add(listaFavoritos);
+
+        } catch (Exception e){
+            return false;
+        }
+        return false;
     }
 
 }
--- src/main/java/logica_negocio/recetas/IControladorRecetasCooker.java
+++ src/main/java/logica_negocio/recetas/IControladorRecetasCooker.java
@@ -1,4 +1,9 @@
 package logica_negocio.recetas;
 
+import entidades.modelo.Receta;
+
 public interface IControladorRecetasCooker {
+
+    public boolean crearListaFavoritos(String nombreLista, String descripcion, Receta receta);
+    public boolean crearListaFavoritos(String nombreLista, String descripcion);
 }
