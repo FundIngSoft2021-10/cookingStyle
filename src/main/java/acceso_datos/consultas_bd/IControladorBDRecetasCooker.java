540df5b07505a1201c361d5d96982a20671eaad0
Estefania Bermudez Arroyo <estefania_bermudeza@javeriana.edu.co>
2021-04-16 12:23:29 -0500

    Create interface IControladorBDRecetasCooker

---
+++ src/main/java/acceso_datos/consultas_bd/IControladorBDRecetasCooker.java
@@ -0,0 +1,23 @@
+package acceso_datos.consultas_bd;
+
+import entidades.dto.DTOListaFavoritos;
+import entidades.modelo.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IControladorBDRecetasCooker {
+    public Ingrediente consultaIngrediente (int idIngrediente) throws SQLException;
+    public List<LineaIngrediente> consultaLineaIngrediente (int idReceta) throws SQLException;
+    public List<PasoReceta> consultaPasosReceta (int idReceta) throws SQLException;
+    public List<Categoria> consultaCategorias (int idReceta) throws SQLException;
+    public Cooker consultaCooker (int idCooker) throws SQLException;
+    public List<Calificacion> consultaCalificaciones (int idReceta) throws SQLException;
+    public MotivoReporte consultaMotivoReporte (int idMotivo) throws SQLException;
+    public List<Reporte> consultaReportes (int idReceta) throws SQLException;
+    public List<Receta> buscarRecetas () throws SQLException;
+    public Receta buscarRecetas (int idreceta) throws SQLException;
+    public boolean crearListaFavoritosConReceta(DTOListaFavoritos listaFavoritos);
+    public boolean crearListaFavoritos(DTOListaFavoritos listaFavoritos);
+
+}
