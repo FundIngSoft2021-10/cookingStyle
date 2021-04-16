730bfd10eb5dd2b4cd9f7fccf9476038411e0b73
Alejandro Morales <a.moralesc@javeriana.edu.co>
2021-04-16 14:41:44 -0500

    Changed FactoryUsuario

--- src/main/java/logica_negocio/registro_autenticacion/FactoryUsuario.java
+++ src/main/java/logica_negocio/registro_autenticacion/FactoryUsuario.java
@@ -1,4 +1,24 @@
 package logica_negocio.registro_autenticacion;
 
+import entidades.modelo.CredencialesUsuario;
+import entidades.modelo.Usuario;
+
+import java.util.Date;
+
 public class FactoryUsuario {
+    public FactoryUsuario() {
+    }
+
+/*
+    private int idUsuario;
+    private String nombreUsuario;
+    private String correo;
+    private Date fechaCreacion;
+    private String nombre;
+    private String apellido;
+    private CredencialesUsuario credenciales;
+    public Usuario crearUsuario(String tipoUsuario, ) {
+
+    }
+    */
 }
