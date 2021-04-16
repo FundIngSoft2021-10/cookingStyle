7976d7267eb738945a67b1ae93af7048f2806ed7
Tatiana Pena <penap_at@javeriana.edu.co>
2021-04-16 15:17:39 -0500

    Creation of Principal And Inicio GUI

--- src/main/java/presentacion/CookingApp.java
+++ src/main/java/presentacion/CookingApp.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class CookingApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("GUI001Principal.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ToGUI001Principal.fxml")));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1600, 900));
         primaryStage.setMaximized(true);
