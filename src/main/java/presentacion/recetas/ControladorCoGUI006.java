e649d6cc1a36adabbfe328dc9c18fc121a84ac97
Alejandro Morales <a.moralesc@javeriana.edu.co>
2021-04-19 17:50:19 -0500

    Start populating CoGUI006

--- src/main/java/presentacion/recetas/ControladorCoGUI006.java
+++ src/main/java/presentacion/recetas/ControladorCoGUI006.java
@@ -1,24 +1,34 @@
 package presentacion.recetas;
 
+import entidades.dto.DTOReceta;
 import entidades.dto.DTOSesion;
+import entidades.dto.Pantalla;
+import entidades.modelo.Cooker;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+import logica_negocio.recetas.ControladorRecetasCooker;
+import logica_negocio.recetas.IControladorRecetasCooker;
 import presentacion.IControladorPantalla;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControladorCoGUI006 implements IControladorPantalla {
     private DTOSesion sesion;
+    private DTOReceta receta;
+    private IControladorRecetasCooker controlRecetas;
 
     @FXML
-    public ImageView fotoReceta;
+    public ImageView imgReceta;
     @FXML
     public Text textNombreReceta;
     @FXML
@@ -54,29 +64,78 @@ public class ControladorCoGUI006 implements IControladorPantalla {
     @Override
     public void inicializar(DTOSesion sesion) {
         this.sesion = sesion;
+        this.controlRecetas = new ControladorRecetasCooker((Cooker) this.sesion.getUsuario());
+
+        // Cargar el perfil
+        this.textNombreUsuario.setText(this.sesion.getUsuario().getNombreUsuario());
+
+        // Cargar la información de la receta
+        this.receta = this.controlRecetas.buscarReceta(this.sesion.getIdReceta());
+
+        // Colocar la información de la receta en la pantalla
+    }
+
+    private void cargarReceta() {
+        this.textNombreReceta.setText(this.receta.getReceta().getNombre());
+        this.textAreaDescripcion.setText(this.receta.getReceta().getDescripcion());
+
+        // Imagen
+        Image imagen;
+        try {
+            imagen = new Image(this.receta.getReceta().getLinkImagen());
+        } catch (Exception e) {
+            imagen = new Image("https://img.icons8.com/pastel-glyph/2x/file-not-found.png");
+        }
+        this.imgReceta.setImage(imagen);
+
+        // Video
+        
+
+        this.cargarChef();
+    }
+
+    private void cargarChef() {
+        this.textNombreChef.setText(this.receta.getAutor().getNombreUsuario());
+
     }
 
     public void clickNombreChef(MouseEvent mouseEvent) {
+        // TODO: Ir perfil chef
     }
 
     public void clickFavorito(MouseEvent mouseEvent) {
+        // TODO: Agregar a favorito
     }
 
     public void clickReportar(MouseEvent mouseEvent) {
+        // TODO: Reportar
     }
 
     public void clickVolver(MouseEvent mouseEvent) {
+        try {
+            this.volverPantalla((Event) mouseEvent, this.sesion);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void clickVerIng(ActionEvent actionEvent) {
+        try {
+            this.cargarPantalla((Event) actionEvent, Pantalla.CO_GUI007_RECETAING, this.sesion, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void clickPerfil(MouseEvent mouseEvent) {
+        // TODO: Perfil
     }
 
     public void clickMembresia(MouseEvent mouseEvent) {
+        // TODO: Membresía
     }
 
     public void clickServicioCliente(MouseEvent mouseEvent) {
+        // TODO: Servicio al cliente
     }
 }
