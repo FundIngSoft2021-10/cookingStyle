5798da508a2e56ef7bacbda543d5fd849f8ffae2
AlejandroMoralesContreras <78283504+AlejandroMoralesContreras@users.noreply.github.com>
2021-04-19 15:20:23 -0500

    Implement feature citas (#7)
    * Create ControladorCitas.java
    * Create DTOAgendaChef.java
    * Create IControladorCitas.java
    * Create ControladorCitas.java
    * Create DTOCitasChef.java
    * Create DTOAgendaChef.java
    * Create ControladorPBDCitas.java
    * Create IControladorPBDCitas.java
    * Create ControladorCitas.java
    * Create funcionalidad consultarDisponibilidadChef
    * Create ControladorCBDCitas.java
    * Create IControladorCBDCitas.java
    * Create IControladorCBDCitas.java
    * Fix issues and increase compatibility
    
    Co-authored-by: Santiago Vasquez

---
+++ src/main/java/acceso_datos/consultas_bd/ControladorCBDCitas.java
@@ -0,0 +1,56 @@
+package acceso_datos.consultas_bd;
+
+import acceso_datos.conexion_bd.ControladorBDConexion;
+import entidades.modelo.Bloque;
+import entidades.modelo.Calendario;
+import entidades.modelo.Chef;
+import entidades.modelo.Dia;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControladorCBDCitas implements IControladorCBDCitas {
+    private ControladorBDConexion controladorBDConexion;
+    private Connection conexion;
+
+    public ControladorCBDCitas() {
+        this.controladorBDConexion = new ControladorBDConexion();
+        conexion = this.controladorBDConexion.conectarMySQL();
+    }
+
+    @Override
+    public Calendario buscarCalendarioChef(Chef chef) throws SQLException {
+        Calendario calendario = new Calendario();
+        String consulta =
+                "SELECT * FROM bloque WHERE Chef_idUsuario = ?";
+
+        try {
+            PreparedStatement stmt = conexion.prepareStatement(consulta);
+            stmt.setBigDecimal(1, new BigDecimal(chef.getIdUsuario()));
+
+            ResultSet rs = stmt.executeQuery();
+
+            List<Bloque> bloques = new ArrayList<>();
+            while (rs.next()) {
+                Bloque bloque = new Bloque();
+
+                bloque.setDia(Dia.valueOf(rs.getInt("dia")));
+                bloque.setHora(rs.getInt("hora"));
+                bloques.add(bloque);
+            }
+            calendario.setBloques(bloques);
+
+        } catch (SQLException sqle) {
+            throw sqle;
+        }
+
+        return calendario;
+    }
+
+
+}
