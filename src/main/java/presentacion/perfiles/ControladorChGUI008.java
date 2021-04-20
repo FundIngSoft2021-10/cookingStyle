d7dd1ecc8a097aeeae4ff883d87df3031872c644
Estefania Bermudez Arroyo <estefania_bermudeza@javeriana.edu.co>
2021-04-20 12:05:24 -0500

    Creation of controller ControladorChGUI008 and ControladorCoGUI010

---
+++ src/main/java/presentacion/perfiles/ControladorChGUI008.java
@@ -0,0 +1,47 @@
+package presentacion.perfiles;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class ControladorChGUI008 {
+    public Text textNombreChef;
+    public Text textCategorias;
+    public Text textEdad;
+    public Text textExperiencia;
+    public Text textEstudios;
+    public Text btmModificarAgenda;
+    public ImageView btnServicioCliente;
+    public ImageView btnEditarImg;
+    public ImageView btnEditarNomChef;
+    public ImageView btnEditarCat;
+    public ImageView btnEditarBiog;
+    public ImageView btnCerrarSesion;
+    public Text btnAgenda;
+    public Text btnVolver;
+    public Button btnAniadirReceta;
+    public ImageView imgReceta;
+    public ImageView btnRecSig;
+    public ImageView btnRecAnt;
+    public Button btnEditar;
+    public Button btnEliminar;
+
+    public void clickServicioCliente(MouseEvent mouseEvent) {
+    }
+
+    public void clickEditarPerfil(MouseEvent mouseEvent) {
+    }
+
+    public void clickCerrar(MouseEvent mouseEvent) {
+    }
+
+    public void clickVolver(MouseEvent mouseEvent) {
+    }
+
+    public void clickEditarReceta(MouseEvent mouseEvent) {
+    }
+
+    public void clickEliminarReceta(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/presentacion/perfiles/ControladorCoGUI010.java
@@ -0,0 +1,4 @@
+package presentacion.perfiles;
+
+public class ControladorCoGUI010 {
+}
