8d13dd293aa40973e5c34bc5b045a308d6d8cd3d
Alejandro Morales <a.moralesc@javeriana.edu.co>
2021-04-21 12:41:38 -0500

    Fix bug where the favourites list for a cooker wasn't persisting in the DB

--- src/main/java/presentacion/CookingApp.java
+++ src/main/java/presentacion/CookingApp.java
@@ -2,9 +2,12 @@ package presentacion;
 
 import entidades.dto.Pantalla;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class CookingApp extends Application {
@@ -12,7 +15,16 @@ public class CookingApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource(Pantalla.TO_GUI001_PRINCIPAL.fxml));
         primaryStage.setTitle("Cooking: Cocina a tu Estilo");
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {    // Fullscreen mediante F11, en cualquier pantalla
+            @Override
+            public void handle(KeyEvent e) {
+                if (e.getCode() == KeyCode.F11) {
+                    primaryStage.setFullScreen(!primaryStage.isFullScreen());
+                }
+            }
+        });
+        primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
