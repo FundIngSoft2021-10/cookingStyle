5de59efd48335f32f2c89e5d73dba03c6ca07bff
DanielCamiloFierroGarcia <dacafiga1@hotmail.com>
2021-05-19 08:37:52 -0500

    Add Eliminar y Modificar Perfil (BD)

--- src/main/java/acceso_datos/persistencia_bd/IControladorPBDRegAut.java
+++ src/main/java/acceso_datos/persistencia_bd/IControladorPBDRegAut.java
@@ -7,5 +7,10 @@ import java.sql.SQLException;
 
 public interface IControladorPBDRegAut {
     public void crearUsuario(TipoUsuario tipoUsuario, Usuario usuario) throws SQLException;
+
     public void crearCredenciales(BigInteger idUsuario, CredencialesUsuario credenciales) throws SQLException;
+
+    public void eliminarPerfil(BigInteger idUsuario) throws SQLException;
+
+    public void modificarPerfil(BigInteger idUsuario, String valorAModificar, String modificacion) throws SQLException;
 }
