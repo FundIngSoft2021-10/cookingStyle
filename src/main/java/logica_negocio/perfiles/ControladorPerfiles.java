457b0efd7a6a778d706008ab9c15ef4b78a0e23d
Estefania Bermudez Arroyo <estefania_bermudeza@javeriana.edu.co>
2021-05-19 19:32:02 -0500

    Create method eliminarPerfil

--- src/main/java/logica_negocio/perfiles/ControladorPerfiles.java
+++ src/main/java/logica_negocio/perfiles/ControladorPerfiles.java
@@ -1,6 +1,10 @@
 package logica_negocio.perfiles;
 
 import acceso_datos.persistencia_bd.ControladorPBDPerfiles;
+import entidades.dto.DTOExito;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
 
 public class ControladorPerfiles implements IControladorPerfiles{
 
@@ -10,5 +14,15 @@ public class ControladorPerfiles implements IControladorPerfiles{
         this.controlPBD = new ControladorPBDPerfiles();
     }
 
+    @Override
+    public DTOExito eliminarPerfil(BigInteger idusuario) throws SQLException {
 
+        try{
+            controlPBD.eliminarPerfil(idusuario);
+            DTOExito mensaje = new DTOExito(true, "¡Se ha eliminado su cuenta!");
+            return mensaje;
+        } catch (SQLException sqlException) {
+            throw sqlException;
+        }
+    }
 }
--- src/main/java/logica_negocio/perfiles/IControladorPerfiles.java
+++ src/main/java/logica_negocio/perfiles/IControladorPerfiles.java
@@ -1,4 +1,11 @@
 package logica_negocio.perfiles;
 
+import entidades.dto.DTOExito;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
+
 public interface IControladorPerfiles {
+
+    public DTOExito eliminarPerfil(BigInteger idusuario) throws SQLException;
 }
