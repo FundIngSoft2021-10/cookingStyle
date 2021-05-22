c1b8a7a6e65d0477cc8e9ddcb4f91e77551145bb
Santiago Vasquez <santiago.vasquezs@javeriana.edu.co>
2021-05-22 13:44:23 -0500

    modificación IControladorPerfiles.java

--- src/main/java/logica_negocio/perfiles/IControladorPerfiles.java
+++ src/main/java/logica_negocio/perfiles/IControladorPerfiles.java
@@ -1,4 +1,9 @@
 package logica_negocio.perfiles;
 
+import entidades.dto.DTOPerfil;
+import entidades.modelo.Usuario;
+
 public interface IControladorPerfiles {
+
+    public DTOPerfil modificarPerfil(Usuario usuario, String valorAModificar, String modificacion );
 }
