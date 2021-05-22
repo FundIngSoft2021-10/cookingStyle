75078a70cf12b005bc82e23034e27789aabc8d52
Santiago Vasquez <santiago.vasquezs@javeriana.edu.co>
2021-05-22 13:37:46 -0500

    modificación ControladorPerfiles.java

--- src/main/java/logica_negocio/perfiles/ControladorPerfiles.java
+++ src/main/java/logica_negocio/perfiles/ControladorPerfiles.java
@@ -1,6 +1,10 @@
 package logica_negocio.perfiles;
 
 import acceso_datos.persistencia_bd.ControladorPBDPerfiles;
+import entidades.dto.DTOPerfil;
+import entidades.modelo.Usuario;
+import java.math.BigInteger;
+import java.sql.SQLException;
 
 public class ControladorPerfiles implements IControladorPerfiles{
 
@@ -10,5 +14,21 @@ public class ControladorPerfiles implements IControladorPerfiles{
         this.controlPBD = new ControladorPBDPerfiles();
     }
 
-
+    public DTOPerfil modificarPerfil(Usuario usuario, String valorAModificar, String modificacion ){
+        try{
+            this.controlPBD.modificarPerfil(usuario.getIdUsuario(),valorAModificar,modificacion);
+        }catch(SQLException e){
+            return new DTOPerfil(false, null,"error en la base de datos");
+        }
+        if(valorAModificar=="correo"){
+            usuario.getCredenciales().setCorreo(modificacion);
+        }
+        if(valorAModificar=="nombreUsuario"){
+            usuario.setNombreUsuario(modificacion);
+        }
+        if(valorAModificar=="nombre"){
+            usuario.setNombre(modificacion);
+        }
+        return new DTOPerfil(true,usuario,"se ha modificado el perfil de manera correcta");
+    }
 }
