afc5fdfe4aa27c5ab35f946c2e0d6ee96d8952fe
Jessica Naizaque <jessicanaizaque1@gmail.com>
2021-05-25 10:27:23 -0500

    Create methods getCorreo

--- src/main/java/acceso_datos/consultas_bd/IControladorCBDCitas.java
+++ src/main/java/acceso_datos/consultas_bd/IControladorCBDCitas.java
@@ -3,8 +3,10 @@ package acceso_datos.consultas_bd;
 import entidades.modelo.Calendario;
 import entidades.modelo.Chef;
 
+import java.math.BigInteger;
 import java.sql.SQLException;
 
 public interface IControladorCBDCitas {
     public Calendario buscarCalendarioChef (Chef chef) throws SQLException;
+    public String getCorreo (BigInteger idUsuario) throws SQLException;
 }
