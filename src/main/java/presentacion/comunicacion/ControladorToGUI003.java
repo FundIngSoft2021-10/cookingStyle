6ed97f1edb6e1a692b41e4e098249c2b1de18ba5
Alejandro Morales <a.moralesc@javeriana.edu.co>
2021-05-26 23:02:33 -0500

    Add functionaly in ToGUI003 to redirect email

--- src/main/java/presentacion/comunicacion/ControladorToGUI003.java
+++ src/main/java/presentacion/comunicacion/ControladorToGUI003.java
@@ -1,12 +1,14 @@
 package presentacion.comunicacion;
 
 import entidades.dto.DTOSesion;
+import javafx.application.HostServices;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import presentacion.IControladorPantalla;
 
 import java.io.IOException;
@@ -14,9 +16,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControladorToGUI003 implements IControladorPantalla {
+    @FXML
     public ImageView btnCorreo;
+    @FXML
     public Text btnVolver;
+    @FXML
     public Text servicioAlClienteTextoSC;
+    @FXML
     private DTOSesion sesion;
 
     @Override
@@ -41,6 +47,9 @@ public class ControladorToGUI003 implements IControladorPantalla {
     public void clickPerfil(MouseEvent mouseEvent) {
     }
 
+    @FXML
     public void clickCorreo(KeyEvent keyEvent) {
+        HostServices hostServices = (HostServices) ((Stage)(this.btnCorreo.getScene().getWindow())).getProperties().get("hostServices");
+        hostServices.showDocument("https://mail.google.com/mail/?view=cm&fs=1&to=soporte.cooking@gmail.com&su=Servicio al Cliente Cooking");
     }
 }
