7cd283edd547497b897f0df6d22290d086395d0b
amoralesc <78283504+amoralesc@users.noreply.github.com>
2021-05-26 20:08:31 -0500

    Implement feature perfiles (#14)
    * Add Eliminar y Modificar Perfil (BD)
    * Create ControladorPerfiles
    * Create IControladorPerfiles
    * Create method eliminarPerfil
    * modificación ControladorPerfiles.java
    * creación DTOPerfil.java
    * modificación ControladorPerfiles.java
    * modificación IControladorPerfiles.java
    * Merge modificarPefil and eliminarPerfil
    * Create directory test and test eliminarPerfilTest
    * Update dependecies
    
    Co-authored-by: DanielCamiloFierroGarcia
    Co-authored-by: Estefania Bermudez Arroyo 
    Co-authored-by: Santiago Vasquez 
    Co-authored-by: Tatiana Pena

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,24 @@ module cooking {
     requires javafx.web;
     requires java.sql;
 
+    opens entidades.dto to org.junit.jupiter;
+    opens entidades.modelo to org.junit.jupiter;
+    opens logica_negocio.perfiles to org.junit.jupiter;
+    opens logica_negocio.recetas to org.junit.jupiter;
+    opens logica_negocio.registro_autenticacion to org.junit.jupiter;
+    opens logica_negocio.citas to org.junit.jupiter;
+    opens logica_negocio.utilidad to org.junit.jupiter;
     opens presentacion to javafx.fxml;
     opens presentacion.registro_autenticacion to javafx.fxml;
     opens presentacion.recetas to javafx.fxml;
     opens presentacion.perfiles to javafx.fxml;
     opens presentacion.comunicacion to javafx.fxml;
     exports presentacion;
+    exports logica_negocio.perfiles;
+    exports logica_negocio.recetas;
+    exports logica_negocio.registro_autenticacion;
+    exports logica_negocio.citas;
+    exports logica_negocio.utilidad;
+    exports entidades.dto;
+    exports entidades.modelo;
 }
